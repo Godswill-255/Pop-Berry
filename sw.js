fa713a97a47113fd42847191faadfc733e615021
Godswill-255 <redthunder0704743@gmail.com>
2025-07-09 08:01:41 +0100

    added sound to work offline

--- sw.js
+++ sw.js
@@ -1,24 +1,26 @@
-const cacheName = 'pop-berry-cache-v1';
+const cacheName = "pop-berry-cache-v1";
 const assets = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/sw.js',
-  '/pop-berry-icon.png'
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/sw.js",
+  "/pop-berry-icon.png",
+  "/bg_Music.mp3",
+  "/Bouble.mp3.mp3",
 ];
 
-self.addEventListener('install', e => {
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(cacheName).then(cache => {
+    caches.open(cacheName).then((cache) => {
       return cache.addAll(assets);
-    })
+    }),
   );
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(res => {
+    caches.match(e.request).then((res) => {
       return res || fetch(e.request);
-    })
+    }),
   );
 });
