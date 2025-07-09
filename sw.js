1fa51d131a00cda412b4e3e8ac1d166b34413bd2
Godswill-255 <redthunder0704743@gmail.com>
2025-07-09 23:35:17 +0100

    added install option and bug fix

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const cacheName = "pop-berry-cache-v1";
+const cacheName = "popberry-v2";
 const assets = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/sw.js",
-  "/pop-berry-icon.png",
-  "/bg_Music.mp3",
-  "/Bouble.mp3.mp3",
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "./pop-berry-icon.png",
+  "./bg_Music.mp3",
+  "./Bouble.mp3",
 ];
 
 self.addEventListener("install", (e) => {
@@ -15,12 +15,31 @@ self.addEventListener("install", (e) => {
       return cache.addAll(assets);
     }),
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== cacheName) return caches.delete(key);
+        }),
+      );
+    }),
+  );
+  return self.clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
+      return (
+        res ||
+        fetch(e.request).catch(
+          () => caches.match("./index.html"), // fallback
+        )
+      );
     }),
   );
 });
